a9e8f8478f0ee0da110c8e92e7dc8b6d4a0fa3c1
mlooe <147048568+mlooe@users.noreply.github.com>
2025-11-13 21:55:47 +0100

    Dodanie lab 6

---
+++ lab6/cfg.php
@@ -0,0 +1,9 @@
+<?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $db = 'moja_strona';
+
+    $link = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+    if (!$link) echo '<b> przerwane polaczenie </b>';
+?>
